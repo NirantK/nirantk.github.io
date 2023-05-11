7a4c3ff3f5921e2381a45023ba88c0001e8240e3
NirantK <nirant.bits@gmail.com>
2023-05-11 08:46:46 +0530

    * refactor(Extraction.py): remove unused import statement * feat(Extraction.py): add print statement to display latest date * refactor(Extraction.py): rearrange code to assign savepath after creating dataframe

--- community_dev/nbs/Extraction.py
+++ community_dev/nbs/Extraction.py
@@ -1,4 +1,3 @@
-import re
 from pathlib import Path
 
 import pandas as pd
@@ -9,15 +8,15 @@ from parsing_utils import MessageExtractor, DataFrameCleaner
 if __name__ == "__main__":
     readpath = Path("_chat.txt")
 
-    # Today's date
-    latest_date = df["Datetime"].max().date().strftime("%Y%m%d")
-    print(f"Today's date is {latest_date}")
-
-    savepath = f"../{latest_date}_Messages.csv"
-
     msg_extractor = MessageExtractor(readpath)
     messages = msg_extractor.extract_messages()
+
     df = pd.DataFrame(messages, columns=["Sender", "Datetime", "Message"])
+
+    latest_date = df["Datetime"].max().date().strftime("%Y%m%d")
+    print(f"Latest date is {latest_date}")
+    savepath = f"../{latest_date}_Messages.csv"
+
     print(f"Before cleanup: {len(df)}")
     df_cleaner = DataFrameCleaner(df)
     df = df_cleaner.cleanup()
