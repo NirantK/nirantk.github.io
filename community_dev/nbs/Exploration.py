4f3b8566d323e19a4e6eee9818da308f9b6b2eb3
NirantK <nirant.bits@gmail.com>
2023-05-11 08:32:58 +0530

    * refactor(Exploration.py): move human_date function to formatting_functions module * refactor(Exploration.py): rename generate_daily_df function to generate_daily_message_df * refactor(Exploration.py): rename Message column to Messages in daily_df

--- community_dev/nbs/Exploration.py
+++ community_dev/nbs/Exploration.py
@@ -1,18 +1,13 @@
 import pandas as pd
-from datetime import date
 
-def human_date(d):
-    def ordinal(n):
-        return "%d%s" % (n, "tsnrhtdd"[((n//10%10!=1)*(n%10<4)*n%10)::4])
+from formatting_functions import human_date
 
-    formatted_date = d.strftime(f"{ordinal(d.day)} %B %Y")
-    return formatted_date
 
 def generate_daily_df(readpath: str, return_df: bool = False) -> None:
     df = pd.read_csv(readpath)
     df["Datetime"] = pd.to_datetime(df["Datetime"])
-    df['Date'] = df['Datetime'].dt.date
-    daily_df = df.groupby('Date').agg({'Message': ' \n '.join}).reset_index()
+    df["Date"] = df["Datetime"].dt.date
+    daily_df = df.groupby("Date").agg({"Message": " \n ".join}).reset_index()
     daily_df["wc"] = daily_df["Message"].apply(lambda x: len(x.split()))
     d = daily_df["Date"][42]
     print(human_date(d))
